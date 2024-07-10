a8e40393741fa6adb1c7d35bd18d00c6306c76df
sumin1s.song <sumin1s.song@samsung.com>
2024-07-10 16:05:20 +1000

    [feature] exception case and guess function

--- game.py
+++ game.py
@@ -1,2 +1,38 @@
 class Game:
-    pass
+    def __init__(self):
+        self.answer = "123"
+    def guess(self, guessNumber):
+        self.assert_illegal_value(guessNumber)
+
+        strike = 0
+        balls = 0
+        solved = 'false'
+
+        for i in range(len(self.answer)):
+            if guessNumber[i] == self.answer[i]:
+                strike +=1
+            elif guessNumber[i] in self.answer:
+                balls +=1
+
+        if strike == len(self.answer):
+            solved = 'true'
+        res = f'solved = {solved}, strikes = {strike}, balls = {balls}'
+
+        return res
+
+    def assert_illegal_value(self, guessNumber):
+        if guessNumber is None:
+            raise TypeError()
+        if len(guessNumber) != 3:
+            raise TypeError()
+        for number in guessNumber:
+            if not ord('0') <= ord(number) <= ord('9'):
+                raise TypeError()
+        if self.isDuplicatedNumber(guessNumber):
+            raise TypeError()
+
+    def isDuplicatedNumber(self, guessNumber):
+        return guessNumber[0] == guessNumber[1] or \
+            guessNumber[0] == guessNumber[2] or \
+            guessNumber[1] == guessNumber[2]
+
--- test_game.py
+++ test_game.py
@@ -1,5 +1,51 @@
 from unittest import TestCase
-
+from game import Game
 class TestGame(TestCase):
-    def test_game(self):
-        pass
+
+    def setUp(self):
+        super().setUp()
+        self.game = Game()
+
+    def assert_illegal_argument(self, guessNumber):
+        try:
+            self.game.guess(guessNumber)
+            self.fail()
+        except TypeError:
+            print('hi')
+            pass
+    def test_exception_when_invalid_input(self):
+        self.assert_illegal_argument(None)
+        self.assert_illegal_argument("12")
+        self.assert_illegal_argument("1234")
+        self.assert_illegal_argument("12s")
+        self.assert_illegal_argument("121")
+
+    def test_return_solve_result_if_matched_number(self):
+        self.game.answer = "123"
+
+        res = self.game.guess(self.game.answer)
+        self.assertEqual("solved = true, strikes = 3, balls = 0",
+                         res)
+
+        res = self.game.guess("456")
+        self.assertEqual("solved = false, strikes = 0, balls = 0",
+                         res)
+
+        res = self.game.guess("129")
+        self.assertEqual("solved = false, strikes = 2, balls = 0",
+                         res)
+
+        res = self.game.guess("240")
+        self.assertEqual("solved = false, strikes = 0, balls = 1",
+                         res)
+
+        res = self.game.guess("321")
+        self.assertEqual("solved = false, strikes = 1, balls = 2",
+                         res)
+
+
+
+
+
+
+
